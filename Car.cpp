106eec9e032a446ba74c6891cee2713c9dc85d53
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2022-10-27 16:24:12 +1030

    Add files via upload

--- Car.cpp
+++ Car.cpp
@@ -19,15 +19,3 @@ Car(int price);   // creates a Car with a price
 Car();
 void drive(int kms);
 };
-
-Car::Car(){
-}
-
-Car::Car(int price){
-	set_emissions=0;
-	set_price=price;
-}
-
-void Car::drive(int kms){
-	set_emissions=kms;
-}
--- Car.h
+++ Car.h
@@ -1,22 +1,7 @@
 #include <iostream>
 #define Car_H
 
-class Car
-{
-private:
-int vinNumber;
-int price;
-int emissions=0;
-
-public:
-int set_vinNumber;
-int set_price;
-int set_emissions=0;
-int get_vinNumber;
-int get_price;
-int get_emissions;
-
-Car(int price);   // creates a Car with a price
+Car::Car(int price);   // creates a Car with a price
 Car();
 void drive(int kms);
 };
