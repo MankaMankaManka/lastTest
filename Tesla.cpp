860167c1aca2861ef27f955460371f6e8287b579
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2022-10-27 16:55:08 +1030

    Add files via upload

---
+++ Tesla.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Car.h"
+#include "Tesla.h"
+
+
+
+Tesla::Tesla(){
+
+}	
+Tesla(char model, int price) {
+	
+}
+  
+char model
+float batteryPercentage      // Initially 100%
+void chargeBattery(int mins);              
---
+++ Tesla.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Car.h"
+#define Tesla_H
+
+class Tesla
+{
+public: 
+class Car;
+Tesla()            
+Tesla(char model, int price)  
+  
+char model
+float batteryPercentage      // Initially 100%
+void chargeBattery(int mins);              
+};
